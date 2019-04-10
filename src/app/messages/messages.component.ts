fb88c34972ca877e41ab8dabc5e49152c794fa1d
lukeguillen <christianguillendrija@gmail.com>
2019-04-10 15:25:18 -0400

    primer commit antes de colocar en repositorio remoto

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MessageService } from '../message.service';
+import {Hero}from '../hero';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+  @Input() hero: Hero;
+  
+  constructor(public messageService: MessageService) {}
+
+  ngOnInit() {
+  }
+
+}
